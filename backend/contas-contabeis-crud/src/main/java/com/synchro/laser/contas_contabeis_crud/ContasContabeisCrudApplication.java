bd4d1d42ce97d4b6845918c4962c66c36dc0f9fb
allanleandrof <alanleandrof@gmail.com>
2025-06-03 11:51:55 -0300

    chore: initial setup spring boot

---
+++ backend/contas-contabeis-crud/src/main/java/com/synchro/laser/contas_contabeis_crud/ContasContabeisCrudApplication.java
@@ -0,0 +1,13 @@
+package com.synchro.laser.contas_contabeis_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasContabeisCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasContabeisCrudApplication.class, args);
+	}
+
+}
---
+++ backend/contas-contabeis-crud/src/test/java/com/synchro/laser/contas_contabeis_crud/ContasContabeisCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synchro.laser.contas_contabeis_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContasContabeisCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
