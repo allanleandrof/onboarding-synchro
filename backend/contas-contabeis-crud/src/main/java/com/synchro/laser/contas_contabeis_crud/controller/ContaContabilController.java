57012678eb55a5e2433550a59fe4af3a3f122524
allanleandrof <alanleandrof@gmail.com>
2025-06-03 19:17:07 -0300

    feat: adiciona controllers

---
+++ backend/contas-contabeis-crud/src/main/java/com/synchro/laser/contas_contabeis_crud/controller/ContaContabilController.java
@@ -0,0 +1,9 @@
+package com.synchro.laser.contas_contabeis_crud.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/conta")
+public class ContaContabilController {
+}
